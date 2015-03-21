147a84760a17a29ec770e742c2bccb926807e149
xiaopz0 <xiaopz0@gmail.com>
2015-03-21 11:50:33 -0400

    minor debug

--- CleanDataAssign/run_analysis.R
+++ CleanDataAssign/run_analysis.R
@@ -10,8 +10,13 @@ X_test<-read.table('X_test.txt')
 
 # combine data
 X_combined<-rbind(X_train, X_test)
-Y_combined<-rbind(Y_train, Y_test)
-all_combined<-cbind(X_combined, Y_combined)
+
+#Uses descriptive activity names to name the activities in the data set
+#Appropriately labels the data set with descriptive variable names. 
+setwd('..')
+
+myvariables <- read.table('features.txt', header=F)
+colnames(X_combined)<- myvariables[,2]
 
 #Extracts only the measurements on the mean and standard deviation for each measurement. 
 mean_ind<-grep('mean[(]',colnames(X_combined))
@@ -21,18 +26,6 @@ std_ind<-grep('std[(]',colnames(X_combined))
 X_std <- X_combined[,std_ind]
 
 X_required = cbind(X_mean, X_std)
-
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive variable names. 
-
-# run analytics
-col_mean<-colMeans(X_combined, na.rm=T)
-col_std<-apply(X_combined, 2, sd)
-
-setwd('..')
-
-myvariables <- read.table('features.txt', header=F)
-colnames(X_combined)<- myvariables[,2]
 
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 setwd('train/')
